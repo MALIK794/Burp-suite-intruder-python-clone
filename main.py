61acbc3a6ff7b56d29253d2268f01d0a4c7e45bd
malik794 <malik.belwaer@gmail.com>
2026-08-17 15:55:13 +0100

    added sniper mode and created the convention for wordlist

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from request_template import template
+from request_sender import send_one
+m=input("what mode would you like to use?\n 1: Sniper\n 2: Pitchfork\n 3: ClusterBomb\nenter the number of mode")
+n=int(input("how many payloads?"))
+if m =="1":
+    try:
+        with open("wordlist/wordlist1.txt") as f1:
+            for a in f1:
+                r=send_one(a.strip(),"http://127.0.0.1:8899/login")
+                print(f"Respone{r}")
+    except FileNotFoundError as e:
+        print(f"WARNING:\nwordlist1 not found\nerror:{e}")
--- request_template.py
+++ request_template.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+def template():
+    template = "long $aah$ string"
+    prefix, _marker, suffix = template.split("$")
 
-template = "long $aah$ string"
-prefix, _marker, suffix = template.split("$")
-
-with open("wordlist.txt", "r") as f1, open("output.txt", "w") as f2:
-    for line in f1:
-        word = line.strip()
-        if not word:
-            continue
-        f2.write(f"{prefix}{word}{suffix}\n")
+    with open("wordlist.txt", "r") as f1, open("output.txt", "w") as f2:
+        for line in f1:
+            word = line.strip()
+            if not word:
+                continue
+            f2.write(f"{prefix}{word}{suffix}\n")
