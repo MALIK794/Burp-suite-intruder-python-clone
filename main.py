797a8ed03a73d0e400f0d270a134cd3c1e34ce72
malik794 <malik.belwaer@gmail.com>
2026-08-18 18:39:36 +0100

    finalized sniper mode

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python3
-from request_template import template
+import sys
+from request_template import *
 from request_sender import send_one
+#note that exit code 0 means the program was executed properly while 1 mean a file was not found and 2 mean a delimiter error
 m=input("what mode would you like to use?\n 1: Sniper\n 2: Pitchfork\n 3: ClusterBomb\nenter the number of mode")
-n=int(input("how many payloads?"))
-ch=input("Input the template")
+ch=input("Input the template \n")
 if m =="1":
-    try:
-        with open("wordlist/wordlist1.txt") as f1:
-            for a in f1:
-                payload=template(ch,a.strip())
-                if payload.count("$")%2==0:
-                    r=send_one(payload,"http://127.0.0.1:8899/login")
+    n=ch.count("$")//2
+    #this block handles the case of ch being empty
+    if ch=="":
+        try:
+            with open(f"wordlist/wordlist1.txt") as f1:
+                for a in f1:
+                    r=send_one(a.strip(),"http://127.0.0.1:8899/login")
                     print(f" Respone{r}")
-                else:
-                    print("string delimiter is invalid")
+            sys.exit(0)
+        except FileNotFoundError as e:
+            print(f"WARNING: wordlist1 not found\nerror:{e}")
+            sys.exit(1)
+    elif n==0:
+        #this block is pretty self-explanatory 
+        print("string contains no delimiter")
+        sys.exit(2)
+    try:
+        #this is the main use case of this program
+        if ch.count("$")%2==0:
+            with open("wordlist/wordlist1.txt") as f1:
+                for a in f1:
+                    for i in range (1,n+1):
+                        print(i)
+                        l=Str2Lst(ch)
+                        payload=CompPayload(l,i*2-1,a.strip())
+                        print(payload)
+                        r=send_one(payload,"http://127.0.0.1:8899/login")
+                        print(f" Respone{r}")
+            sys.exit(0)
+        else:
+            print("string delimiter is invalid")
+            sys.exit(2)
     except FileNotFoundError as e:
         print(f"WARNING:\nwordlist1 not found\nerror:{e}")
+        sys.exit(1)
--- request_template.py
+++ request_template.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
-def template(ch,word):
+def template(ch):
+    prefix, _marker, suffix = ch.split("$",2)
+    return prefix, _marker, suffix
+
+def sniper(ch):
     if ch!="":
         prefix, _marker, suffix = ch.split("$",2)
-        return(f"{prefix}{word}{suffix}\n")
-    else:
-        return word.strip()
+        return(f"{_marker}\n")
+def Str2Lst(ch):
+    l=[]
+    suffix=""
+    while ch.count("$")!=0:
+        preffix, _marker, suffix = template(ch)
+        l.extend([preffix,_marker])
+        ch=suffix
+    l.append(suffix)
+    return l
+def CompPayload(l,n,word):
+    ch=""
+    for i in range (len(l)):
+        if i!=n :
+            ch+=l[i]
+        else:
+            ch+=f"{word}"
+    return ch
+print (Str2Lst("this is very $cool$ and $skibbidy$"))
